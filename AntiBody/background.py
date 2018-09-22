039cdba2d930ad6a2c1f570d794fbb3fe7b9e4df
Joe O'Regan <a00258304@student.ait.ie>
2018-09-22 01:30:20 +0100

    added ship and 1 laser firing

--- AntiBody/background.py
+++ AntiBody/background.py
@@ -23,19 +23,79 @@ DS = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Antibody - Scrolling Background")
 FPS = 120
 
+# define colours
+BLACK = (0,0,0,255)
+WHITE = (255,255,255,255)
+
+# Sprites & Images
 bgImage = pygame.image.load("background.png").convert()
+playerImage = pygame.image.load("Player1Ship.png").convert()
+laserImage = pygame.image.load("LaserGreen.png").convert()
+
 x = 0
+
+circleRadius = 25
+circlePosX = circleRadius
+
+playerPosX = circleRadius
+playerPosY = 585
+playerVelocityX, playerVelocityY = 0, 0
+
+laserX, laserY = 0,0
+laserActive = False
+
+SPEED = 5
 
 # main loop
 while True:
     events()
 
+    # Scrolling Background
     rel_x = x % bgImage.get_rect().width
     DS.blit(bgImage, (rel_x - bgImage.get_rect().width, 0))
     if rel_x < width:
         DS.blit(bgImage, (rel_x, 0))
     x -= 1
 
-    #pygame.draw.line(DS, 255,0,0), (rel_x, 0)
+    # Keyboard input
+    k = pygame.key.get_pressed()
+    if k[K_RIGHT]:
+        playerVelocityX = 1
+    elif k[K_LEFT]:
+        playerVelocityX = -1
+    else:
+        playerVelocityX = 0
+
+    if k[K_UP]:
+        playerVelocityY = -1
+    elif k[K_DOWN]:
+        playerVelocityY = 1
+    else:
+        playerVelocityY = 0
+
+    if k[K_SPACE] and not laserActive:
+        laserActive = True
+        laserX = playerPosX
+        laserY = playerPosY + (playerImage.get_rect().height / 2)   # Fire from center of ships height
+
+
+    # Laser
+    if laserActive:
+        laserX += 10
+        DS.blit(laserImage, (laserX, laserY))
+        if laserX > width:
+            laserActive = False
+
+
+    # Update Player Position
+    playerPosX += playerVelocityX * SPEED
+    playerPosY += playerVelocityY * SPEED
+
+    # pygame.draw.circle(DS, WHITE, (playerPosX, playerPosY - circleRadius), circleRadius, 0)
+    DS.blit(playerImage, (playerPosX, playerPosY))
+
+
+    # pygame.draw.line(DS, 255,0,0), (rel_x, 0)
     pygame.display.update()
     CLOCK.tick(FPS)
+    DS.fill(BLACK)
