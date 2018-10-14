1aa6fc90a5b5ec81a7fa162b79a7a10f94342431
Joe O'Regan <a00258304@student.ait.ie>
2018-10-15 00:13:45 +0100

    Week 1 assignment trig 1

---
+++ Wk1/Trig.py
@@ -0,0 +1,25 @@
+'''
+Joe O'Regan
+A00258304
+Wk1 Assignment
+Part 1: Trig 2
+'''
+
+import math as m
+
+
+def trigFunction(angle, distance):
+    height = distance * (m.tan(m.radians(angle)))
+    return height
+
+
+def main():
+    d = float(input("Enter the distance to object: "))
+    a = float(input('Enter Angle of Elevation: '))
+    h = trigFunction(a, d)
+    #print('Height of object is: %s' %format(float(h), '0.2E'))
+    print('Height of object is: %.2f' %h)
+
+
+if __name__ == "__main__":
+    main()
