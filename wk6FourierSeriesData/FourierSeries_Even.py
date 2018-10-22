165459af1aeef4046ace1ce5a6eb5a779289a5a0
Joe O'Regan <joeaoregan@yahoo.ie>
2018-10-22 15:11:45 +0100

    fourier series

--- wk6FourierSeriesData/FourierSeries_Even.py
+++ wk6FourierSeriesData/FourierSeries_Even.py
@@ -2,14 +2,14 @@ import numpy as np  # answer is in plot title of graph
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 
-def Odd(k, b, L, t):    # t is the time that changes
+def Even(k, b, L, t):    # t is the time that changes
     result = 0  # set result to 0
     if k<=0:
         return result
-    result+= (b/(k*np.pi))*np.sin((k*np.pi/L)*t)+Odd(k-2,b, L, t)   # 20 / kPi(Sin(kPi/L * t), call function again - only thing that changes is the term number
+    result+= (b/(k*np.pi)) * np.sin((k*np.pi/L)*t) + Even(k - 2, b, L, t)   # 20 / kPi(Sin(kPi/L * t), call function again - only thing that changes is the term number
     return result       # done for just one point at this time
 
-def oddExample(data):
+def evenExample(data):
     k = int(input("Enter the number of terms you want - odd number only: "))    # enter odd number of terms
     L = 5.0                                                                     # upper limit of 5
     a0 = 14.0
@@ -23,11 +23,11 @@ def oddExample(data):
     x_data = []
     y_data = []
 
-    text_file = open("Odd_Fourier_Data.csv", "w")  # as we calc each point in the series, put in texxt file
+    text_file = open("Even_Fourier_Data.csv", "w")  # as we calc each point in the series, put in texxt file
     for i in range(size):
         x_data.append(LL)                       # append with position on the limit
-        y_data.append(a0/2+Odd(k,b,L,LL))       # a0 = dc average, k = num terms, b = 20, limits -- appended to y axis at the same point time is appended
-        text_file.write("{0:0.5f} , {1:0.5f} \n".format(LL, Odd(k, b, L, LL)))  # write to text file, start at 5 and increase as we go
+        y_data.append(a0 / 2 + Even(k, b, L, LL))       # a0 = dc average, k = num terms, b = 20, limits -- appended to y axis at the same point time is appended
+        text_file.write("{0:0.5f} , {1:0.5f} \n".format(LL, Even(k, b, L, LL)))  # write to text file, start at 5 and increase as we go
         LL+=step
     text_file.close()
 
@@ -45,12 +45,12 @@ def oddExample(data):
 
 
 def loadData():     # no need for option here
-    x, y = np.genfromtxt('Odd_Data.csv', unpack=True, delimiter=',')    # open csv file, comma used as delimiter to separate data
+    x, y = np.genfromtxt('Even_Data.csv', unpack=True, delimiter=',')    # open csv file, comma used as delimiter to separate data
     return (x, y)   # x into x array, y into y array
 
 def main():
-    print("Fourier Series Plots")
-    oddExample(loadData())      # no need to input 1
+    print("Fourier Series Plots - Even")
+    evenExample(loadData())      # no need to input 1
 
 
 if __name__=="__main__":
